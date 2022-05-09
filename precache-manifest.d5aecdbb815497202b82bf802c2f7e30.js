f4e5cad0ade890e468ff03aff513dc494f16f5ce
phantom <rostandmessi2@gmail.com>
2022-05-09 18:26:39 +0100

    Updates

---
+++ precache-manifest.d5aecdbb815497202b82bf802c2f7e30.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "03beafaad81908ad8db3",
+    "url": "/Building_Website/static/css/main.2226a380.chunk.css"
+  },
+  {
+    "revision": "03beafaad81908ad8db3",
+    "url": "/Building_Website/static/js/main.03beafaa.chunk.js"
+  },
+  {
+    "revision": "17fb29ed40cd8c1083bf",
+    "url": "/Building_Website/static/js/1.17fb29ed.chunk.js"
+  },
+  {
+    "revision": "a93930a63129c44fba24",
+    "url": "/Building_Website/static/css/2.f9e98a2a.chunk.css"
+  },
+  {
+    "revision": "a93930a63129c44fba24",
+    "url": "/Building_Website/static/js/2.a93930a6.chunk.js"
+  },
+  {
+    "revision": "b57b7ef90aadede78e23",
+    "url": "/Building_Website/static/js/runtime~main.b57b7ef9.js"
+  },
+  {
+    "revision": "83d885226d5c9de5c597b6167741d426",
+    "url": "/Building_Website/static/media/house1.83d88522.jpg"
+  },
+  {
+    "revision": "006c7a1a8daa1601ba59657304707a77",
+    "url": "/Building_Website/static/media/house2.006c7a1a.jpg"
+  },
+  {
+    "revision": "7c083811f8949a45c0889a7d7f2742a6",
+    "url": "/Building_Website/static/media/house3.7c083811.jpg"
+  },
+  {
+    "revision": "9259e9d8e3559d905de2eb3e14951881",
+    "url": "/Building_Website/static/media/house4.9259e9d8.jpg"
+  },
+  {
+    "revision": "9bdd9c00cf9ab209ca2f87be9d70996e",
+    "url": "/Building_Website/static/media/architect.9bdd9c00.jpg"
+  },
+  {
+    "revision": "4a9e5b0e8e95e8efbe25bb2070ad1ebf",
+    "url": "/Building_Website/static/media/team2.4a9e5b0e.jpg"
+  },
+  {
+    "revision": "96ed83da474793a517201961d60ad326",
+    "url": "/Building_Website/static/media/team3.96ed83da.jpg"
+  },
+  {
+    "revision": "7d607d8b571f84509304e351996858ab",
+    "url": "/Building_Website/static/media/team4.7d607d8b.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Building_Website/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Building_Website/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Building_Website/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Building_Website/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Building_Website/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "40b20bb29a353a443856ede42867099e",
+    "url": "/Building_Website/index.html"
+  }
+];
